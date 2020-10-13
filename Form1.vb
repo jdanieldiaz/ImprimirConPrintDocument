99c9590278d19e55b5ca5130cd32939c9b354353
dandia28@gmail.com <jdanieldiaz>
2020-10-13 14:14:05 -0300

    Agregar archivos de proyecto.

---
+++ Form1.vb
@@ -0,0 +1,35 @@
+﻿Imports System.Drawing.Printing
+Public Class Form1
+
+    Private Sub print_PrintPage(ByVal sender As Object,
+                            ByVal e As PrintPageEventArgs)
+        ' Este evento se producirá cada vez que se imprima una nueva página
+        ' imprimir HOLA MUNDO en Arial tamaño 24 y negrita
+
+        ' imprimimos la cadena en el margen izquierdo
+        Dim xPos As Single = e.MarginBounds.Left
+        ' La fuente a usar
+        Dim prFont As New Font("Arial", 24, FontStyle.Bold)
+        ' la posición superior
+        Dim yPos As Single = prFont.GetHeight(e.Graphics)
+
+        ' imprimimos la cadena
+        e.Graphics.DrawString("Hola, Mundo", prFont, Brushes.Black, xPos, yPos)
+        ' indicamos que ya no hay nada más que imprimir
+        ' (el valor predeterminado de esta propiedad es False)
+        e.HasMorePages = False
+
+    End Sub
+
+
+    Private Sub cmdImprimir_Click(sender As Object, e As EventArgs) Handles cmdImprimir.Click
+        ' ejemplo simple para imprimir en .NET
+        ' Usamos una clase del tipo PrintDocument
+        Dim printDoc As New PrintDocument
+        ' asignamos el método de evento para cada página a imprimir
+        AddHandler printDoc.PrintPage, AddressOf print_PrintPage
+        ' indicamos que queremos imprimir
+        printDoc.Print()
+    End Sub
+
+End Class
---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' La información general de un ensamblado se controla mediante el siguiente 
+' conjunto de atributos. Cambie estos valores de atributo para modificar la información
+' asociada con un ensamblado.
+
+' Revisar los valores de los atributos del ensamblado
+
+<Assembly: AssemblyTitle("EjemploImpresión")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("EjemploImpresión")>
+<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+<Assembly: Guid("52a8ed7d-a5a3-46c7-9254-fd4e71906ca4")>
+
+' La información de versión de un ensamblado consta de los cuatro valores siguientes:
+'
+'      Versión principal
+'      Versión secundaria
+'      Número de compilación
+'      Revisión
+'
+' Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
+' utilizando el carácter "*", como se muestra a continuación:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
